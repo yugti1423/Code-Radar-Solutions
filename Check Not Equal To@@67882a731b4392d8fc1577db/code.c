efa3a73aa0b5021d8a86befe98cd9abfbd70864b
yugti1423 <yugti0081.becse24@chitkara.edu.in>
2025-02-05 10:58:42 +0530

    Solution to Check Not Equal To

--- Check Not Equal To@@67882a731b4392d8fc1577db/code.c
+++ Check Not Equal To@@67882a731b4392d8fc1577db/code.c
@@ -3,11 +3,11 @@
 void not_equal(){
     int x,y;
     scanf("%d %d",&x, &y);
-    if(x != y){
-        printf("True")
+    if(x == y){
+        printf("False")
     }
     else{
-        printf("False");
+        printf("True");
     }
 }
 int main(){
