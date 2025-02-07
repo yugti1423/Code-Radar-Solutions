e9a1ef254593d1091eef58ee7e9b8442a376694b
Yugti <yugti0081.becse24@chitkara.edu.in>
2025-02-07 23:32:29 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -7,7 +7,7 @@ void func(){
         printf("Vowels");
     }else if(ch=='b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'){
         printf("Consonant");
-    } else if(ch=='?','&','*','::'){
+    } else if(ch=='?','&','*',':'){
         printf("Special Charaacters");
     }else{
         printf("Digit");
