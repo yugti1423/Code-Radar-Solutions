83b15adacde0744f75e9beafc1f0e52faea6ed5a
Yugti <yugti0081.becse24@chitkara.edu.in>
2025-02-08 08:59:38 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <ctype.h>
 
-void func(){
+int main() {
     char ch;
-    scanf("%c",&ch);
-    if(ch == 'a','e','i','o','u','A','E','I','O','U'){
-        printf("Vowel");
-    }else if(ch=='b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'){
-        printf("Consonant");
-    } else if(ch=='?','&','*',':'){
-        printf("Special Charaacters");
-    }else{
-        printf("Digit");
-    }
-}
+    scanf("%c", &ch);
 
-int main(){
-    func();
+    if (isalpha(ch)) {
+        if (ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o' || ch == 'U' || ch == 'u') {
+            printf("%cVowel", ch);
+        } else {
+            printf("%cConsonant", ch);
+        }
+    }
+    else if (isdigit(ch)) {
+        printf("%cDigit", ch);
+    }
+    else {
+        printf("%cSpecial character", ch);
+    }
     return 0;
 }
