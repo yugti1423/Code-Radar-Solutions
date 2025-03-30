ebbac1fd2c83bbc52f059c2dcbc4c3738972e67d
Yugti <yugti0081.becse24@chitkara.edu.in>
2025-03-30 21:43:58 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

---
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void findRank(int n, int leaderboard[], int m, int gameScores[]) {
+    
+    int uniqueLeaderboard[n];
+    int uniqueSize = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || leaderboard[i] != leaderboard[i - 1]) {
+            uniqueLeaderboard[uniqueSize++] = leaderboard[i];
+        }
+    }
+    
+    int index = uniqueSize - 1;
+    for (int i = 0; i < m; i++) {
+
+        while (index >= 0 && gameScores[i] >= uniqueLeaderboard[index]) {
+            index--;
+        }
+            
+            printf("%d\n", index + 2);
+    }
+}
+
+int main(){
+    //n=leaderboard size...
+    //m=number of games played...
+    int n,m;
+    scanf("%d",&n);
+    int ranked[2000000];
+    for(int i=0, i<n; i++) scanf("%d",ranked[i]);
+
+    scanf("%d",&m);
+    int player[2000000];
+    for(int i=0; i < m; i++) scanf("%d",&player[i]);
+
+    int result[2000000];
+    trackPlayerRanks(n,player,m,result);
+    for(int i=0; i<m; i++) printf("%d\n",result[i]);
+
+    return 0;
+}
